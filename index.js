31f24f4a983ba7b6d0f81df046de9a4234dd57c9
Pavel Dziamentsyeu <paveldz@mail.com>
2020-06-10 22:42:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, obj, {[key]: value})
   var obj = { prop: 1 }
+  return Object.assign({}, obj, {[key]: value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
